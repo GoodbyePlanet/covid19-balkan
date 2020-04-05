12cf6da01ae86a59138ac961220965a3ad4dbbbb
Nemanja <nemanja.vasic@codecentric.de>
2020-04-05 22:11:23 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: /node_modules/,
         use: {
           loader: "babel-loader?cacheDirectory",
           options: {
