1f77b30a8de66991eb1c73c657d3d662be97dd82
Nemanja <nemanja.vasic@codecentric.de>
2020-04-06 19:45:47 +0200

    Update webpack config and add homepage propeerty in package json

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,11 @@
 const $path = require("path");
 
-// const isProd = process.env.NODE_ENV === "production";
-// const outputFilename = isProd ? "[name].[contenthash].js" : "name.[hash].js";
-
 module.exports = {
   mode: "production",
 
   devtool: "source-map",
 
-  // entry: {
-  //   index: "./src/index.js",
-  // },
-
   entry: ["./src/index.js", "./src/styles/main.css"],
-
-  // output: {
-  //   path: $path.join(__dirname, "dist"),
-  //   filename: "[name].js",
-  //   chunkFilename: "[name].js",
-  //   publicPath: "/dist/",
-  // },
 
   output: {
     path: $path.join(__dirname, "dist"),
@@ -41,7 +27,7 @@ module.exports = {
         test: /\.js$/,
         include: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader?cacheDirectory",
           options: {
             presets: ["@babel/preset-env"],
             plugins: ["@babel/plugin-syntax-dynamic-import"],
