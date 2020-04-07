f687dd29f8f73b75115e5beaaee7836a1def1b69
Nemanja <nemanja.vasic@codecentric.de>
2020-04-07 23:55:05 +0200

    Delete comments

--- src/index.js
+++ src/index.js
@@ -184,7 +184,6 @@ window.onload = function () {
     modal.style.display = "none";
   };
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
