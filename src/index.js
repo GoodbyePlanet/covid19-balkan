d21ae7050d4815ed1851da8c3b75a7b3e92c7833
Nemanja <nemanja.vasic@codecentric.de>
2020-04-11 00:53:26 +0200

    Add basic logarithmic chart

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import regeneratorRuntime from "regenerator-runtime";
 import { NovelCovid } from "novelcovid";
 import { CountUp } from "countup.js";
-import { ready, useTheme, percent, create } from "@amcharts/amcharts4/core";
+import {
+  ready,
+  useTheme,
+  percent,
+  create,
+  color,
+  Scrollbar,
+} from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4themes_dark from "@amcharts/amcharts4/themes/dark";
@@ -11,16 +18,112 @@ import { tooltip } from "./tooltip";
 
 const covidApi = new NovelCovid();
 
-start();
+// getHistoricalDataFoCountry('Serbia');
+
+startLogarithmicChart();
+startRadarChart();
 printTotalCountsOnBalkan();
 
-function start() {
+function createAxisAndSeries(field, name) {
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis()); // 1
+  valueAxis.logarithmic = true; // 2
+  valueAxis.renderer.minGridDistance = 20; // 3
+
+  // Create series
+  let series = chart.series.push(new am4charts.LineSeries());
+  series.dataFields.valueY = field;
+  series.dataFields.dateX = "date";
+  series.tensionX = 0.8;
+  series.strokeWidth = 3;
+  series.yAxis = valueAxis;
+  series.name = name;
+  series.tooltipText = "Cases: [bold]{valueY}[/]";
+  series.tensionX = 0.8;
+  series.showOnInit = true;
+
+  let bullet = series.bullets.push(new am4charts.CircleBullet());
+  bullet.circle.fill = color("#fff");
+  bullet.circle.strokeWidth = 3;
+}
+
+function startLogarithmicChart() {
   ready(async function () {
     useTheme(am4themes_dark);
     useTheme(am4themes_dataviz);
     useTheme(am4themes_animated);
 
-    let chart = create("chartdiv", am4charts.RadarChart);
+    var chart = create("logarithmicChart", am4charts.XYChart);
+
+    chart.dateFormatter.dateFormat = "yyyy/MM/dd";
+    // Add data
+    // chart.data = getLogarithmicData();
+
+    chart.data = await getHistoricalDataFoCountry("Serbia");
+    // chart.colors.step = 2;
+
+    // Create axes
+    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+    dateAxis.renderer.minGridDistance = 50;
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis()); // 1
+    valueAxis.logarithmic = true; // 2
+    valueAxis.renderer.minGridDistance = 20; // 3
+
+    // Create series
+    var series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "cases";
+    series.dataFields.dateX = "date";
+    series.tensionX = 0.8;
+    series.strokeWidth = 3;
+    series.yAxis = valueAxis;
+    series.name = "RS Cases";
+    series.tooltipText = "Cases: [bold]{valueY}[/]";
+    series.tensionX = 0.8;
+    series.showOnInit = true;
+
+    var bullet = series.bullets.push(new am4charts.CircleBullet());
+    bullet.circle.fill = color("#fff");
+    bullet.circle.strokeWidth = 3;
+
+    chart.legend = new am4charts.Legend();
+
+    // Add cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.fullWidthLineX = true;
+    // chart.cursor.fullWidthLineY = true;
+    chart.cursor.xAxis = dateAxis;
+    // chart.cursor.yAxis = valueAxis;
+    chart.cursor.lineX.strokeWidth = 0;
+    chart.cursor.lineX.fill = color("#000");
+    chart.cursor.lineX.fillOpacity = 0.1;
+
+    // Add scrollbar
+    chart.scrollbarX = new Scrollbar();
+
+    // Add a guide
+    let range = valueAxis.axisRanges.create();
+    range.value = 90.4;
+    range.grid.stroke = color("#396478");
+    range.grid.strokeWidth = 1;
+    range.grid.strokeOpacity = 1;
+    range.grid.strokeDasharray = "3,3";
+    range.label.inside = true;
+    // range.label.text = "Average";
+    // range.label.fill = range.grid.stroke;
+    // range.label.verticalCenter = "bottom";
+  });
+}
+
+function startRadarChart() {
+  ready(async function () {
+    useTheme(am4themes_dark);
+    useTheme(am4themes_dataviz);
+    useTheme(am4themes_animated);
+
+    let chart = create("radarChart", am4charts.RadarChart);
+
+    chart.responsive.enabled = true;
 
     chart.data = renameCountryNames(await getData());
     chart.innerRadius = percent(40);
@@ -146,6 +249,84 @@ async function printTotalCountsOnBalkan() {
   } catch (error) {
     console.error("Error has occured", error);
   }
+}
+
+function getLogarithmicData() {
+  return [
+    {
+      date: "2020/03/21",
+      price: 5,
+    },
+    {
+      date: "2020/03/22",
+      price: 12,
+    },
+    {
+      date: "2020/03/23",
+      price: 19,
+    },
+    {
+      date: "2020/03/24",
+      price: 35,
+    },
+    {
+      date: "2020/03/25",
+      price: 46,
+    },
+    {
+      date: "2020/03/26",
+      price: 48,
+    },
+    {
+      date: "2020/03/27",
+      price: 55,
+    },
+    {
+      date: "2020/03/28",
+      price: 65,
+    },
+    {
+      date: "2020/03/29",
+      price: 75,
+    },
+    {
+      date: "2020/03/30",
+      price: 83,
+    },
+    {
+      date: "2020/03/31",
+      price: 103,
+    },
+    {
+      date: "2020/04/5",
+      price: 135,
+    },
+    {
+      date: "2020/04/6",
+      price: 171,
+    },
+    {
+      date: "2020/04/7",
+      price: 222,
+    },
+    {
+      date: "2020/04/8",
+      price: 249,
+    },
+  ];
+}
+
+async function getHistoricalDataFoCountry(country) {
+  const historical = await covidApi.historical(null, country);
+
+  const cases = historical.timeline.cases;
+
+  const historicData = Object.entries(cases).map((item) => ({
+    date: item[0],
+    cases: item[1],
+  }));
+
+  return historicData;
 }
 
 window.onload = async function () {
